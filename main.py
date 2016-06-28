bf3f3261597bda5a6c5836192a64fdd862af6032
Qumeric <qumidium@gmail.com>
2016-06-28 17:02:22 +0300

    Add templates

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
