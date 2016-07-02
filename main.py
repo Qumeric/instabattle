88e37d5b22cc1f933cf76d27b785d828e99269c0
Qumeric <qumidium@gmail.com>
2016-07-02 09:33:47 +0300

    Add database (not used ATM)

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
+import os
 from flask import Flask, render_template, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap, WebCDN
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'temp' # FIXME
-manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
+manager = Manager(app)
+db = SQLAlchemy(app)
+
 app.extensions['bootstrap']['cdns']['jquery'] = WebCDN(
     '//cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/' # Use jquery 3.0.0
 )
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return "<Role {}>".format(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+
 
 class RegisterForm(Form):
     email = StringField('Email', validators=[Required(), Email()])
@@ -32,5 +60,6 @@ def register():
 
 
 if __name__ == '__main__':
+    db.create_all()
     manager.run()
 
