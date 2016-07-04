eb1ecb2d97c2ebc6269b79684bce3adcec7875a5
Qumeric <qumidium@gmail.com>
2016-07-04 19:43:08 +0300

    fake users generator

--- app/models.py
+++ app/models.py
@@ -89,6 +89,27 @@ class User(UserMixin, db.Model):
             lazy='dynamic')
     avatar_hash = db.Column(db.String(32))
 
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(email=forgery_py.internet.email_address(),
+                     username=forgery_py.internet.user_name(True),
+                     password=forgery_py.lorem_ipsum.word(),
+                     confirmed=True,
+                     name=forgery_py.name.full_name(),
+                     about_me=forgery_py.lorem_ipsum.sentence(),
+                     member_since=forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         if self.role is None:
